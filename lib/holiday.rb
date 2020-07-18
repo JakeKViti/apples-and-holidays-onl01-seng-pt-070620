a9dc8f1c47b0ba5a0d28d9281066a86f302e197f
Jacob Viti <thevitij64@gmail.com>
2020-07-18 06:45:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
       if supply.include?("BBQ")
         bbqHolidays.unshift(holiday)
       end
-      return bbqHolidays
     end
   end
+  return bbqHolidays
 end
