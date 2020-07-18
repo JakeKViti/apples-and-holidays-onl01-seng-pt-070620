3ae176c7de41a583685a56ee580c0eb1a53f84e5
Jacob Viti <thevitij64@gmail.com>
2020-07-18 05:12:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_supplies.each do |season, holiday|
     if season == "winter"
-      puts "#{holiday}"
+      holiday_supplies[:winter][holiday] << supply
     end
   end
 end
