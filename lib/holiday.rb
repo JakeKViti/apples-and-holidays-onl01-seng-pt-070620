fbfe223e844b8d981b2fa71054e199d185272eb9
Jacob Viti <thevitij64@gmail.com>
2020-07-18 06:44:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supply|
       if supply.include?("BBQ")
-        bbqHolidays.unshift(holday)
+        bbqHolidays.unshift(holiday)
       end
     end
   end
